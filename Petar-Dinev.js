a08b508286432c609bb93bb5a0722285c7074750
bezanski <bezanski@abv.bg>
2023-09-20 22:40:13 +0300

    updated Petar-Dinev.js

--- Petar-Dinev.js
+++ Petar-Dinev.js
@@ -2,6 +2,14 @@ function helloEveryone() {
     let me = {
         name: "Petar",
         age: 27,
-        hobbies: ["running", "hiking", "fitness"]
+        hobbies: ["running", "hiking", "fitness"],
+        town: "Sofia",
     }
+    let empty = "";
+    for(i=1; i<=10; i++) {
+        empty += i;
+    }
+    console.log(empty);
 }
+
+helloEveryone()
