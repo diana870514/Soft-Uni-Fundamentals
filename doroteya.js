cb6010ba1006293419d905db4b013c0ce812fac5
SnowShine7 <82907196+SnowShine7@users.noreply.github.com>
2023-09-20 14:52:06 +0300

    added a little funtion

--- doroteya.js
+++ doroteya.js
@@ -1,3 +1,13 @@
-let name = Doroteya;
-let age = 21;
-let hobbies = [music,walks,games]
+// let name = Doroteya;
+// let age = 21;
+// let hobbies = [music,walks,games]
+
+function solve(a,b) {
+    for (let i = 1;i <= a; i++) {
+        for (let j = 1; j <= b; j++) {
+            console.log(`${i} : ${j}`)
+        }
+    }
+}
+
+solve(10, 5)
